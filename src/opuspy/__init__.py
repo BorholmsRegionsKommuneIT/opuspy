a82a043f0b45265c86100dab08aec59fa6305cff
Dominik Zdyb <dominik2711@gmail.com>
2024-09-05 10:31:07 +0200

    only export start opus in __init__

--- src/opuspy/__init__.py
+++ src/opuspy/__init__.py
@@ -1,23 +1,7 @@
-from .brkrpautils import (
-    send_email,
-    TOPdeskIncidentClient,
-    backup_old_password,
-    generate_new_password,
-    save_new_password,
-    get_credentials,
-    parse_ri_html_report_to_dataframe,
+from .opus import (
     start_opus,
-    start_ri,
 )
 
 __all__ = [
-    "send_email",
-    "TOPdeskIncidentClient",
-    "backup_old_password",
-    "generate_new_password",
-    "save_new_password",
-    "get_credentials",
-    "parse_ri_html_report_to_dataframe",
     "start_opus",
-    "start_ri",
 ]
